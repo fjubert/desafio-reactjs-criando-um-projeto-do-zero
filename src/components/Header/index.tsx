1b5651e5a4644e756e4457a27392c4468a30e25f
fjubert <felipe.ajubert@gmail.com>
2021-11-16 00:44:49 -0300

    fix: Header not recognized by tests

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,16 @@
+import Link from 'next/link';
 import styles from './header.module.scss';
 import commonStyles from '../../styles/common.module.scss';
 
-export function Header(): JSX.Element {
+export default function Header(): JSX.Element {
   return (
     <header className={`${styles.headerContainer} ${commonStyles.container}`}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="logo" />
+        <Link href="/">
+          <a>
+            <img src="/images/spacetraveling.svg" alt="logo" />
+          </a>
+        </Link>
       </div>
     </header>
   );
